f4bca650196b57412c995271f1c33c7b8065e70a
Masdupp <oscarmasdup@gmail.com>
2023-04-20 01:14:22 +0200

    did the shape part and almost finished id's

--- shapes.c
+++ shapes.c
@@ -1,2 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "shapes.h"
+#include "line.h"
+#include "circle.h"
+#include "square.h"
+#include "rectangle.h"
+#include "polygon.h"
 
+Shape *create_empty_shape(SHAPE_TYPE shape_type)
+{
+    Shape *shp = (Shape *) malloc(sizeof(Shape));
+    shp->ptrShape = NULL;
+    shp->id = 1;
+    shp->shape_type = shape_type;
+    return shp;
+}
+
+Shape *create_point_shape(int px, int py)
+{
+    Shape *shp = create_empty_shape(POINT);
+    Point *p = create_point(px, py);
+    shp->ptrShape = p;
+    return shp;
+}
+
+Shape *create_line_shape(int px1, int py1, int px2, int py2){
+    Shape *shp = create_empty_shape(LINE);
+    Point *p1 = create_point(px1, py1);
+    Point *p2 = create_point(px2, py2);
+    Line *line = create_line(p1, p2);
+    shp ->ptrShape = line;
+    return shp
+}
+
+Shape *create_square_shape(int px, int py, int length){
+    Shape *shp = create_empty_shape(SQUARE);
+    Point *p = create_point(px,py);
+    Square *square = create_square(p, length);
+    shp-> ptrShape = square;
+    return shp
+}
+
+Shape *create_rectangle_shape(int px, int py, int width, int height){
+    Shape *shp = create_empty_shape(RECTANGLE);
+    Point *p = create_point(px, py);
+    Rectangle *rectangle = create_rectangle(p, width, height);
+    shp->ptrShape = rectangle;
+    return shp;
+}
+
+Shape *create_circle_shape(int px, int py, int radius){
+    Shape *shp = create_empty_shape(CIRCLE);
+    Point *p = create_point(px, py);
+    Circle *circle = create_circle(p, radius);
+    shp-> ptrShape = circle;
+    return shp;
+}
+
+Shape *create_polygon_shape(int lst[], int n){
+    if(n%2 !=0){
+        printf("Error : Number of points must be even\n");
+        return NULL;
+    }
+    Shape *shp = create_empty_shape(POLYGON);
+    Polygon *polygon = create_polygon(n);
+    shp-> ptrShape = polygon;
+    return shp;
+}
+void delete_shape(Shape * shape){
+    if(shape){
+        if(shape->ptrShape){
+            free(shape->ptrShape);
+        }
+        free(shape);
+    }
+    }
+void print_shape(Shape * shape){
+    printf("%c,%d %d %d %d",&shape)
+
+}
