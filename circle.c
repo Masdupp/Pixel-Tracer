e52f0f78d42cd568d9380ae6db3fa616632ac093
Masdupp <oscarmasdup@gmail.com>
2023-04-19 19:27:51 +0200

    Created circle

---
+++ circle.c
@@ -0,0 +1,16 @@
+#include "circle.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+Circle *create_circle(Point * center, int radius){
+    Circle  *circle= (Circle*) malloc(sizeof(circle));
+    circle->point = center;
+    circle->radius = radius;
+    return circle;
+}
+void delete_circle(Circle * circle){
+    free(circle);
+}
+void print_circle(Circle * circle){
+    
+}
