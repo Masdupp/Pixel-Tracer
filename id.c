db2a6b2e70858351a5eb5d65d2e844fa9843e954
Masdupp <oscarmasdup@gmail.com>
2023-04-20 19:20:36 +0200

    continu

---
+++ id.c
@@ -0,0 +1,6 @@
+#include "id.h"
+
+unsigned int global_id = 0;
+unsigned int get_next_id(global_id){
+
+}
---
+++ id.h
@@ -0,0 +1,9 @@
+#ifndef CPROJECT_ID_H
+#define CPROJECT_ID_H
+unsigned int global_id;
+global_id = 0;
+unsigned int get_next_id(global_id){
+    
+}
+
+#endif //CPROJECT_ID_H
