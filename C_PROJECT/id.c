13bb83d51656dd6627e752501b68eefe0464adfd
Masdupp <oscarmasdup@gmail.com>
2023-05-23 23:32:00 +0200

    final version

--- C_PROJECT/id.c
+++
@@ -1,5 +0,0 @@
-#include "id.h"
-
-unsigned int get_next_id(){
-    return id_counter++;
-    }
--- C_PROJECT/id.h
+++
@@ -1,7 +0,0 @@
-#ifndef CPROJECT_ID_H
-#define CPROJECT_ID_H
-
-static unsigned int id_counter = 1;
-
-unsigned int get_next_id();
-#endif //CPROJECT_ID_H
